37d1ef20a9519f3c1fb6b9e54b2dd3b1a6a04a54
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-05-24 12:25:07 +0530

    Update SQL_Server_Queries.sql

--- SQL_Server_Queries.sql
+++ SQL_Server_Queries.sql
@@ -17,5 +17,26 @@ CREATE TABLE Employee(
 	PRIMARY KEY (emp_ID)
 );
 
+--INSERT COMMAND
+INSERT INTO Employee (emp_ID, emp_Name, emp_Salary, emp_age, emp_Gender, emp_Dept) VALUES
+(101, 'Alice', 50000, 28, 'Female', 'HR'),
+(102, 'Bob', 60000, 32, 'Male', 'IT'),
+(103, 'Charlie', 55000, 30, 'Male', 'Finance'),
+(104, 'Diana', 62000, 27, 'Female', 'Marketing'),
+(105, 'Ethan', 48000, 24, 'Male', 'Support'),
+(106, 'Fiona', 70000, 35, 'Female', 'IT'),
+(107, 'George', 58000, 29, 'Male', 'HR'),
+(108, 'Hannah', 53000, 31, 'Female', 'Finance'),
+(109, 'Ian', 49000, 26, 'Male', 'Support'),
+(110, 'Jasmine', 75000, 33, 'Female', 'Marketing');
 
+
+--SELECT STATEMENTS
 SELECT * FROM Employee;
+
+--WHERE CLAUSE
+SELECT * FROM Employee
+WHERE emp_Gender = 'Female';
+
+SELECT * FROM Employee
+WHERE emp_Salary > 60000;
